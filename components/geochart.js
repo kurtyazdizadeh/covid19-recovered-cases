a0d6d6bff63b953f2c6f47968df5b5d4dab25539
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-03-24 18:02:40 -0700

    covid stats are grabbed through ajax call and stored in app.js. Sent data to geochart.js and renders data of USA map to the DOM. Need to update stateList.js

--- components/geochart.js
+++ components/geochart.js
@@ -5,61 +5,8 @@ class GeoChart {
     this.drawMap = this.drawMap.bind(this);
 
     this.mapElement = mapElement;
-    this.stateList = {
-      'US-AL': 'Alabama',
-      'US-AK': 'Alaska',
-      'US-AZ': 'Arizona',
-      'US-AR': 'Arkansas',
-      'US-CA': 'California',
-      'US-CO': 'Colorado',
-      'US-CT': 'Connecticut',
-      'US-DE': 'Delaware',
-      'US-DC': 'District Of Columbia',
-      'US-FL': 'Florida',
-      'US-GA': 'Georgia',
-      'US-HI': 'Hawaii',
-      'US-ID': 'Idaho',
-      'US-IL': 'Illinois',
-      'US-IN': 'Indiana',
-      'US-IA': 'Iowa',
-      'US-KS': 'Kansas',
-      'US-KY': 'Kentucky',
-      'US-LA': 'Louisiana',
-      'US-ME': 'Maine',
-      'US-MD': 'Maryland',
-      'US-MA': 'Massachusetts',
-      'US-MI': 'Michigan',
-      'US-MN': 'Minnesota',
-      'US-MS': 'Mississippi',
-      'US-MO': 'Missouri',
-      'US-MT': 'Montana',
-      'US-NE': 'Nebraska',
-      'US-NV': 'Nevada',
-      'US-NH': 'New Hampshire',
-      'US-NJ': 'New Jersey',
-      'US-NM': 'New Mexico',
-      'US-NY': 'New York',
-      'US-NC': 'North Carolina',
-      'US-ND': 'North Dakota',
-      'US-OH': 'Ohio',
-      'US-OK': 'Oklahoma',
-      'US-OR': 'Oregon',
-      'US-PA': 'Pennsylvania',
-      'US-RI': 'Rhode Island',
-      'US-SC': 'South Carolina',
-      'US-SD': 'South Dakota',
-      'US-TN': 'Tennessee',
-      'US-TX': 'Texas',
-      'US-UT': 'Utah',
-      'US-VT': 'Vermont',
-      'US-VA': 'Virginia',
-      'US-WA': 'Washington',
-      'US-WV': 'West Virginia',
-      'US-WI': 'Wisconsin',
-      'US-WY': 'Wyoming'
-    }
   }
-  loadGoogleChart() {
+  loadGoogleChart(stateList, data) {
     google.charts.load(
       'current',
       {
@@ -67,16 +14,21 @@ class GeoChart {
         'mapsApiKey': googleMaps_APIKey
       }
     );
-    google.charts.setOnLoadCallback(this.drawMap);
-
+    google.charts.setOnLoadCallback(() => {
+      this.drawMap(stateList, data)
+    });
   }
-  drawMap() {
+  drawMap(stateList, data) {
     var stateArray = [
       ['State', 'Persons Recovered']
     ]
 
-    for (var states in this.stateList){
-      stateArray.push([this.stateList[states], 53])
+    for (var states in stateList){
+      for(var i = 0; i < data.stats.breakdowns.length; i++){
+        if (states === data.stats.breakdowns[i].location.isoCode){
+          stateArray.push([stateList[states], data.stats.breakdowns[i].totalRecoveredCases])
+        }
+      }
     }
 
     var data = google.visualization.arrayToDataTable(stateArray);
--- components/stateList.js
+++ components/stateList.js
@@ -1,5 +1,5 @@
 class StateList {
   constructor(stateListElement) {
     this.stateListElement = stateListElement;
+    }
   }
-}
