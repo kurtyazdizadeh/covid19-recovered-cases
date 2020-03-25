40a36f6060369702bd6173ae68003eff9212b6ab
Kurt Yazdizadeh <kurt.yazdizadeh@gmail.com>
2020-03-24 17:27:10 -0700

    added dummy data for geoChart, displays data for all 50 states. Need to pull actual number of recovered persons from covid API and put into geochart.drawMap()

--- components/covid.js
+++ components/covid.js
@@ -5,4 +5,26 @@ class CovidStats {
   constructor(statsElement) {
     this.statsElement = statsElement;
   }
+
 }
+
+
+
+// $.ajax({
+//   type: "GET",
+//   url: "https://api.smartable.ai/coronavirus/stats/US",
+
+//   // Request headers
+//   beforeSend: function (xhrObj) {
+//     xhrObj.setRequestHeader("Cache-Control", "no-cache");
+//     xhrObj.setRequestHeader("Subscription-Key", "79badf971fec458f9e3012fe0a6aedc0");
+//   },
+// })
+//   .done(function (data) {
+//     alert("success");
+//     console.log("COVID-19 data", data);
+//   })
+//   .fail(function (error) {
+//     alert("error");
+//     console.log(error);
+//   });
--- components/geochart.js
+++ components/geochart.js
@@ -2,7 +2,62 @@ var googleMaps_APIKey = 'AIzaSyDhYugfpMscV09jwbTry1YKDUNGhtfh9PI';
 
 class GeoChart {
   constructor(mapElement) {
+    this.drawMap = this.drawMap.bind(this);
+
     this.mapElement = mapElement;
+    this.stateList = {
+      'US-AL': 'Alabama',
+      'US-AK': 'Alaska',
+      'US-AZ': 'Arizona',
+      'US-AR': 'Arkansas',
+      'US-CA': 'California',
+      'US-CO': 'Colorado',
+      'US-CT': 'Connecticut',
+      'US-DE': 'Delaware',
+      'US-DC': 'District Of Columbia',
+      'US-FL': 'Florida',
+      'US-GA': 'Georgia',
+      'US-HI': 'Hawaii',
+      'US-ID': 'Idaho',
+      'US-IL': 'Illinois',
+      'US-IN': 'Indiana',
+      'US-IA': 'Iowa',
+      'US-KS': 'Kansas',
+      'US-KY': 'Kentucky',
+      'US-LA': 'Louisiana',
+      'US-ME': 'Maine',
+      'US-MD': 'Maryland',
+      'US-MA': 'Massachusetts',
+      'US-MI': 'Michigan',
+      'US-MN': 'Minnesota',
+      'US-MS': 'Mississippi',
+      'US-MO': 'Missouri',
+      'US-MT': 'Montana',
+      'US-NE': 'Nebraska',
+      'US-NV': 'Nevada',
+      'US-NH': 'New Hampshire',
+      'US-NJ': 'New Jersey',
+      'US-NM': 'New Mexico',
+      'US-NY': 'New York',
+      'US-NC': 'North Carolina',
+      'US-ND': 'North Dakota',
+      'US-OH': 'Ohio',
+      'US-OK': 'Oklahoma',
+      'US-OR': 'Oregon',
+      'US-PA': 'Pennsylvania',
+      'US-RI': 'Rhode Island',
+      'US-SC': 'South Carolina',
+      'US-SD': 'South Dakota',
+      'US-TN': 'Tennessee',
+      'US-TX': 'Texas',
+      'US-UT': 'Utah',
+      'US-VT': 'Vermont',
+      'US-VA': 'Virginia',
+      'US-WA': 'Washington',
+      'US-WV': 'West Virginia',
+      'US-WI': 'Wisconsin',
+      'US-WY': 'Wyoming'
+    }
   }
   loadGoogleChart() {
     google.charts.load(
@@ -16,15 +71,15 @@ class GeoChart {
 
   }
   drawMap() {
-    var data = google.visualization.arrayToDataTable([
-      ['State', 'Persons Recovered'],
-      ['California', 200],
-      ['NV', 300],
-      ['FL', 400],
-      ['TX', 500],
-      ['AZ', 600],
-      ['NY', 700]
-    ]);
+    var stateArray = [
+      ['State', 'Persons Recovered']
+    ]
+
+    for (var states in this.stateList){
+      stateArray.push([this.stateList[states], 53])
+    }
+
+    var data = google.visualization.arrayToDataTable(stateArray);
 
     var options = {
       region: 'US',
